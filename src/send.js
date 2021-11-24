e25c2ec5a0797f6d5f74d8372c32985ca11b7b65
tuesi <tadas.a.1793@gmail.com>
2021-11-24 13:06:24 +0200

    page changed

--- src/send.js
+++ src/send.js
@@ -1,8 +1,6 @@
 export function SendEmail(email){
-    // fetch("https://gyvunuerotika.herokuapp.com/subscribe?email=" + email)
-    // .then(res => res.json())
-    // .then((result) => {console.log(result)});
-    return fetch("http://localhost:80/subscribe?email=" + email)
+    //return fetch("http://localhost:80/subscribe?email=" + email)
+    return fetch("https://gyvunuerotika.herokuapp.com/subscribe?email=" + email)
     .then(res => res.json())
     .then(result => {return result.status});
 }
