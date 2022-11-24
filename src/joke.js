9a281dfac10b86520a89b74d6557df9360ceff57
Adomaitis Tadas <tadas.adomaitis@mif.stud.vu.lt>
2022-11-24 15:49:15 +0200

    user environment variables

--- src/joke.js
+++ src/joke.js
@@ -1,21 +1,21 @@
 import './Joke.css';
 import Button from 'react-bootstrap/Button';
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { Modal } from 'react-bootstrap';
 const botUrl = "https://discord.com/api/oauth2/authorize?client_id=864559583597953044&permissions=1393851362368&scope=applications.commands%20bot";
 
 function Joke() {
 
     useEffect(() => {
-        fetch("https://dienos-juokas-backend.onrender.com/daily")
-        .then(res => res.json())
-        .then(result => {
-            if(result.joke != null){
-            setJoke(result.joke)
-            }
-        })
-        .catch((error) => {});
-    },[]);
+        fetch(process.env.BACKEND_LINK + "/daily")
+            .then(res => res.json())
+            .then(result => {
+                if (result.joke != null) {
+                    setJoke(result.joke)
+                }
+            })
+            .catch((error) => { });
+    }, []);
 
     const [show, setShow] = useState(false);
     const [joke, setJoke] = useState('Dienos juokas rodomas nuo 13:00. Norint dienos juoką gauti anksčiau prašome prenumeruoti.');
--- src/send.js
+++ src/send.js
@@ -1,6 +1,7 @@
-export function SendEmail(email){
-    return fetch("https://dienos-juokas-backend.onrender.com/subscribe?email=" + email)
-    .then(res => res.json())
-    .then(result => {return result.status})
-    .catch((error) => {return -2});
+
+export function SendEmail(email) {
+    return fetch(process.env.BACKEND_LINK + "/subscribe?email=" + email)
+        .then(res => res.json())
+        .then(result => { return result.status })
+        .catch((error) => { return -2 });
 }
